6a00095563a3a8d6f1f1bcb8584ddb04291da46c
jeremy wambugu <wambugujeremy@gmail.com>
2020-03-22 17:19:03 +0300

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener("submit", e => {
 
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
-  fetch(`http://localhost:3000/weather?address=${location}`)
+  fetch(`/weather?address=${location}`)
     .then(res => {
       return res.json();
     })
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public");
@@ -20,73 +21,73 @@ hbs.registerPartials(partialsPath);
 app.use(express.static(publicDirPath));
 
 app.get("", (req, res) => {
-  res.render("index", {
-    title: "Weather App",
-    name: "Jasmine Wambugu"
-  });
+	res.render("index", {
+		title: "Weather App",
+		name: "Jasmine Wambugu"
+	});
 });
 app.get("/about", (req, res) => {
-  res.render("about", {
-    title: "About Me",
-    name: "Jasmine Wambugu"
-  });
+	res.render("about", {
+		title: "About Me",
+		name: "Jasmine Wambugu"
+	});
 });
 app.get("/help", (req, res) => {
-  res.render("help", {
-    message: "How can we be of assitance",
-    title: "Help",
-    name: "Jeremy Wambugu"
-  });
+	res.render("help", {
+		message: "How can we be of assitance",
+		title: "Help",
+		name: "Jeremy Wambugu"
+	});
 });
 
 app.get("/weather", (req, res) => {
-  if (!req.query.address) {
-    return res.send({
-      error: "You must provide an address!"
-    });
-  }
+	if (!req.query.address) {
+		return res.send({
+			error: "You must provide an address!"
+		});
+	}
 
-  geocode(
-    req.query.address,
-    (error, { latitude, longitude, location } = {}) => {
-      if (error) {
-        return res.send({ error });
-      }
+	geocode(
+		req.query.address,
+		(error, { latitude, longitude, location } = {}) => {
+			if (error) {
+				return res.send({ error });
+			}
 
-      forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-          return res.send({ error });
-        }
+			forecast(latitude, longitude, (error, forecastData) => {
+				if (error) {
+					return res.send({ error });
+				}
 
-        res.send({
-          forecast: forecastData,
-          location,
-          address: req.query.address
-        });
-      });
-    }
-  );
+				res.send({
+					forecast: forecastData,
+					location,
+					address: req.query.address
+				});
+			});
+		}
+	);
 });
 
 app.get("/products", (req, res) => {
-  console.log(req.query.search);
-  if (!req.query.search) {
-    return res.send({ error: "You must provide a search query" });
-  }
-  console.log(req.query.search);
-  res.send({
-    products: []
-  });
+	console.log(req.query.search);
+	if (!req.query.search) {
+		return res.send({ error: "You must provide a search query" });
+	}
+	console.log(req.query.search);
+	res.send({
+		products: []
+	});
 });
 
 app.get("*", (req, res) => {
-  res.render("404", {
-    title: "404",
-    name: "Jeremy wambugu",
-    errorMessage: "Page not found"
-  });
+	res.render("404", {
+		title: "404",
+		name: "Jeremy wambugu",
+		errorMessage: "Page not found"
+	});
 });
 
-app.listen(3000, () => {
-  console.log(`Server started on port`);
+app.listen(port, () => {
+	console.log(`Server started on port ${port} `);
 });
